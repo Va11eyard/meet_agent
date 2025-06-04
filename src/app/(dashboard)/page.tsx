af8b49e3b746bffb841fa95172f106db489f4fcb
Va11eyard <mynzhasardimash@gmail.com>
2025-06-04 11:35:27 +0500

    trpc setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,11 +1,13 @@
 import { auth } from "@/lib/auth";
 
 import { HomeView } from "@/modules/home/ui/views/home-view";
+import { caller } from "@/trpc/server";
 
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 const Page = async () => {
+
   const session = await auth.api.getSession({
     headers: await headers(),
   })
@@ -13,7 +15,8 @@ const Page = async () => {
   if (!session) {
     redirect("/sign-in");
   }
+
   return <HomeView />
 }
- 
+
 export default Page;
