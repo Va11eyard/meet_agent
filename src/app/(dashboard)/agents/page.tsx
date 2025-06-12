ab069b5d432f7f49cd14e76fc8f05fcb85b9d32b
Va11eyard <mynzhasardimash@gmail.com>
2025-06-12 12:13:07 +0500

    Agents seatch and pagination

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 
 import { auth } from "@/lib/auth";
 
+import { loadSearchParams } from "@/modules/agents/params";
 import { AgentsView, AgentsViewError, AgentsViewLoading } from "@/modules/agents/ui/views/agents-view";
 import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
 
@@ -14,8 +15,16 @@ import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-const Page = async () => {
-    
+import type { SearchParams } from "nuqs";
+
+interface Props {
+    searchParams: Promise<SearchParams>;
+}
+
+const Page = async ({ searchParams }: Props) => {
+
+    const filters = await loadSearchParams(searchParams); 
+
     const session = await auth.api.getSession({
         headers: await headers(),
     })
@@ -25,7 +34,9 @@ const Page = async () => {
     }
 
     const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({
+        ...filters,
+    }));
 
     return (
         <>
